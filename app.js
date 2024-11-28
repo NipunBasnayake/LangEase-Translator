a9e46bbc4cf6d1a5a7d642f6ae1d9e8a22a4f6d8
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-11-28 15:16:32 +0530

    UI enhanced

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ selectTag.forEach((tag, id) => {
         let selected;
         if (id == 0 && countyCode == "en-GB") {
             selected = "selected";
-        } else if (id == 1 && countyCode == "si-LK"){
+        } else if (id == 1 && countyCode == "si-LK") {
             selected = "selected";
         }
         let option = `<option value= "${countyCode}" ${selected}>${countries[countyCode]}</option>`
@@ -17,25 +17,25 @@ selectTag.forEach((tag, id) => {
 let apiResponce;
 
 function translates() {
-
     let txtInputText = document.getElementById("txtInputText").value;
     let select1Value = document.getElementById("select1").value;
     let select2Value = document.getElementById("select2").value;
 
+    document.getElementById('loadingSpinner').style.display = 'block';
+
     const requestOptions = {
         method: "GET",
         redirect: "follow"
-      };
-      
-      fetch(`https://api.mymemory.translated.net/get?q=${txtInputText}!&langpair=${select1Value}|${select2Value}`)
+    };
+
+    fetch(`https://api.mymemory.translated.net/get?q=${txtInputText}&langpair=${select1Value}|${select2Value}`)
         .then((response) => response.json())
         .then((result) => {
-            // console.log(result.responseData.translatedText)
             apiResponce = result.responseData.translatedText;
-            // console.log(apiResponce);
-            displayBody = `${apiResponce}`;
-            outputText.innerHTML = displayBody;
+            outputText.value = apiResponce;
         })
-        .catch((error) => console.error(error));
+        .catch((error) => console.error(error))
+        .finally(() => {
+            document.getElementById('loadingSpinner').style.display = 'none';
+        });
 }
-
